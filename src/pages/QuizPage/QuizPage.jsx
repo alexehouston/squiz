d8b24cae913cab744fe31580b4994ce487160d35
alexehouston <alexehouston@gmail.com>
2022-12-19 11:59:54 -0600

    deployment

--- src/pages/QuizPage/QuizPage.jsx
+++ src/pages/QuizPage/QuizPage.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect, Link } from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
 import Questionaire from '../../components/Questionaire/Questionaire';
 import * as questionsApi from '../../utilities/question-api';
 import "./QuizPage.css";
